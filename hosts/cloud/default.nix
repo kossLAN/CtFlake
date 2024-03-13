4f888e55255692d053e074a363149dd0db162857
kossLAN <kosslan@kosslan.dev>
2024-03-13 13:28:15 -0400

    test: more testing on nextcloud/syncthing

--- hosts/cloud/default.nix
+++ hosts/cloud/default.nix
@@ -58,7 +58,7 @@
       maxUploadSize = "51200M";
       https = true;
 
-      phpExtraExtensions = all: [ all.smbclient ];
+      phpExtraExtensions = all: [ all.smbclient all.inotify ];
 
       phpOptions = {
         "opcache.interned_strings_buffer" = 16;
@@ -100,8 +100,6 @@
     syncthing = {
       enable = true;
       guiAddress = "0.0.0.0:8384";
-      user = "nextcloud";
-      group = "nextcloud";
     };
   };
 
@@ -110,10 +108,9 @@
     ln -fs $systemConfig/init /sbin/init
   '';
 
-  # users.users.syncthing = {
-  #   initialPassword = "root";
-  #   extraGroups = [ "nextcloud" ];
-  # };
+  users.users.nextcloud = {
+    extraGroups = [ "syncthing" ];
+  };
 
   users.users.root.initialPassword = "root";
   users.users.root.openssh.authorizedKeys.keys = [ ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpkeLOreGeqUDLcrlYgzyeSSZmBvJLY+dWOeORIpGQQVRvlko8NRcVKS/fa5EHBd9HG9gRs96FK5WF9JJCGsY4ovL++WZwlsQN3xfc0xq2Sn8TQhgDgiBFCR05JDMi1+f6v9WpaiLiQnOKiTmSGYhzvayIr/XrpcAaXo0mLDEnqZbSzqTcAcqZMcPZixmkgFJA+kUq6d1Z5XMPRRTPJNmLGY0jNbVlUiI9pWsIlGqZFcMLssNWnIZkl8SCV/lN+uyFy2G1o1LlMQ6UFziqP3Zm28gq6alt7ivFJ8A8hUffiZWeQ4uURV8TKhQ43FGSUspma7DpG5zGdionkN521rQJajdnWJLO25dXRkDdXWmkwpFuKRep0m0xv0VSxXAPYs5IrFuDuylbfo6W0N5dx2sPgBK8cQ2uj5AvVCM6g8cgWh+pxzG/WV/2XpwrT7jD8vyRUL+U6FpiMQIsepJ/WQIhA7HkQnex2QHGAsu7hP5Wr5Bs33m8JYT5XCT0KsXkzQE= koss@galahad'' ];
