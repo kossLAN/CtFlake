70944d8c4ffe2eb982e6ec869981708931fe7eba
kossLAN <kosslan@kosslan.dev>
2024-03-13 20:23:42 -0400

    feat: stablize nextcloud conf

--- hosts/cloud/default.nix
+++ hosts/cloud/default.nix
@@ -39,6 +39,21 @@
     allowedUDPPorts = [ 21027 22000 ];
   };
 
+  systemd.services."inotify-nextcloud" = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    description = "Run inotify watcher for nextcloud.";
+    serviceConfig = {
+      Restart = "on-failure";
+      SuccessExitStatus = "3 4";
+      RestartForceExitStatus = "3 4";
+      Type = "notify";
+      User = "nextcloud";
+      Group = "nextcloud";
+      ExecStart = "nextcloud-occ files_external:notify -v 1";
+    };
+  };
+
   services = {
     openssh = {
       enable = true;
@@ -100,6 +115,9 @@
     syncthing = {
       enable = true;
       guiAddress = "0.0.0.0:8384";
+      user = "nextcloud";
+      group = "nextcloud";
+      dataDir = "/var/lib/nextcloud";
     };
   };
 
@@ -107,10 +125,6 @@
     mkdir -p /sbin
     ln -fs $systemConfig/init /sbin/init
   '';
-
-  users.users.nextcloud = {
-    extraGroups = [ "syncthing" ];
-  };
 
   users.users.root.initialPassword = "root";
   users.users.root.openssh.authorizedKeys.keys = [ ''ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpkeLOreGeqUDLcrlYgzyeSSZmBvJLY+dWOeORIpGQQVRvlko8NRcVKS/fa5EHBd9HG9gRs96FK5WF9JJCGsY4ovL++WZwlsQN3xfc0xq2Sn8TQhgDgiBFCR05JDMi1+f6v9WpaiLiQnOKiTmSGYhzvayIr/XrpcAaXo0mLDEnqZbSzqTcAcqZMcPZixmkgFJA+kUq6d1Z5XMPRRTPJNmLGY0jNbVlUiI9pWsIlGqZFcMLssNWnIZkl8SCV/lN+uyFy2G1o1LlMQ6UFziqP3Zm28gq6alt7ivFJ8A8hUffiZWeQ4uURV8TKhQ43FGSUspma7DpG5zGdionkN521rQJajdnWJLO25dXRkDdXWmkwpFuKRep0m0xv0VSxXAPYs5IrFuDuylbfo6W0N5dx2sPgBK8cQ2uj5AvVCM6g8cgWh+pxzG/WV/2XpwrT7jD8vyRUL+U6FpiMQIsepJ/WQIhA7HkQnex2QHGAsu7hP5Wr5Bs33m8JYT5XCT0KsXkzQE= koss@galahad'' ];
